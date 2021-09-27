0ac3c682472e9b0e2060773527a8bc5ee948e572
Donny <donvoowright@aol.com>
2021-09-27 17:20:55 -0600

    Update main_unit_tests.py

--- src/unit_tests/main_unit_tests.py
+++ src/unit_tests/main_unit_tests.py
@@ -3,5 +3,6 @@ import src.pyion.main as main
 
 
 class MainTestCase(unittest.TestCase):
+    #Runner doesn't return anything
     def test_runner_returns_none(self):
         self.assertIsNone(main.runner())
