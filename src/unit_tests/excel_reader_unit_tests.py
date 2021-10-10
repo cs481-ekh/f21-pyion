d043ef216af77338834927c363dcc935b5c59601
Donny208 <donvoowright@aol.com>
2021-10-10 13:29:53 -0600

    adding more unit tests

--- src/unit_tests/excel_reader_unit_tests.py
+++ src/unit_tests/excel_reader_unit_tests.py
@@ -9,10 +9,22 @@ class ExcelReaderTestCase(unittest.TestCase):
         with self.assertRaises(Exception):
             er.get_voltages(None, 1)
 
+    #get_voltages fails on null
     def test_voltages_throws_error_given_null_row(self):
         sheet_mock = Mock()
         with self.assertRaises(Exception):
             er.get_voltages(sheet_mock, 1)
+
+    # get_v_add fails on null
+    def test_v_add_throws_error_given_null_sheet(self):
+        with self.assertRaises(Exception):
+             er.get_v_add(None, 1)
+
+    # get_v_add fails on null
+    def test_v_add_throws_error_given_null_row(self):
+        sheet_mock = Mock()
+        with self.assertRaises(Exception):
+            er.get_v_add(sheet_mock, 1)
 
     def test_read_file_throws_error_given_null_file(self):
         with self.assertRaises(Exception):
