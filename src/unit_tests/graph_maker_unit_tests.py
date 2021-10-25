766ac9cb6f671d29d5d0017f92df8f49cf9f154b
Donovan Wright <donovanwright@u.boisestate.edu>
2021-10-24 20:06:42 -0600

    implements task #32

---
+++ src/unit_tests/graph_maker_unit_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from src.pyion.graph_maker import *
+from src.pyion.objects.PyionUnit import PyionUnit
+
+class GraphMakerTestCase(unittest.TestCase):
+
+    # Data source none throws exception
+    def test_graph_single_throws_issue_when_none_given(self):
+        with self.assertRaises(Exception):
+            graph_single(None)
+
+    def test_graph_single_throws_issue_when_data_is_int(self):
+        with self.assertRaises(Exception):
+            data = PyionUnit("test", "none", 1)
+            graph_single(data)
+
+
