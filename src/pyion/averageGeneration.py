82c2324dc93cc3513b8dd7ef5a8891806e01e77b
Sagechen1 <Chrisschenzixiao@gmail.com>
2021-11-11 15:34:49 -0700

    Fixed averageGeneration.py, and modified PyionData.py

--- src/pyion/averageGeneration.py
+++ src/pyion/averageGeneration.py
@@ -7,13 +7,14 @@ def calculateAverage(input):
             return None
 
     sublists = [input[n:n+3] for n in range(0, len(input), 3)]  # sublists contains sublists of length 3, subList is in the format of [[a,b,c],[c,d,e]...]
-    total = 0   #this is the total/sum of a block of three
+    #total = 0   #this is the total/sum of a block of three
 
     averagelist = [] #this is the list that holds all the calculated average
 
     #head = ["Average Voltage"] #this is the header for the table that stores average value
 
     for item in sublists: # every item is a sublist in the format of [x,y,z]
+        total = 0
         for i in item:  #every i is a number in sublist, like x in [x,y,z]
                 total = total + i   # calculate the total of a sublist like [x,y,z]
         average = total/3 #for every item/sublist of three elements, calculate the average
